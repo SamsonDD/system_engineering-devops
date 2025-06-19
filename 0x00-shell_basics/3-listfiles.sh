de8d5645ea2cee8b5ae76f8428495e744fbd2729
SamsonDD <samsoneworking@gmail.com>
2025-06-19 20:11:08 +0300

    Update and rename 3-listfiles.sh to 3-listfiles

--- 0x00-shell_basics/3-listfiles.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo -e "total 16\n-rwxr-xr-x 1 996 992 16 0-current_working_directory\n-rw-r--r-- 1 996 992 16 0x00-shell_basics\n-rwxr-xr-x 1 996 992 15 1-listit\n-rw-r--r-- 1 996 992 198 3-listfiles" chmod +x 2-script_name.sh
